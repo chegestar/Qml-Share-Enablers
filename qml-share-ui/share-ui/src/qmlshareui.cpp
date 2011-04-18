2d077983034931d0e10392f3ade6aa68b455e2d2
Alexander Bokovoy <alexander.bokovoy@nokia.com>
2011-04-18 16:33:05 +0300

    Update headers to follow share-ui, these two files were overlooked during initial commit

--- qml-share-ui/share-ui/src/qmlshareui.cpp
+++ qml-share-ui/share-ui/src/qmlshareui.cpp
@@ -1,15 +1,25 @@
-/* This file is part of nokia-share-ui
+/*
+ * This file is part of Handset UX Share user interface
  *
- * Copyright © 2010 Nokia Corporation and/or its subsidiary(-ies).
- * All rights reserved.
- * Contact: Jukka Tiihonen <jukka.t.tiihonen@nokia.com>
+ * Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies). All rights reserved.
+ * Contact: Jukka Tiihonen <jukka.tiihonen@nokia.com>
  *
- * This software, including documentation, is protected by copyright controlled
- * by Nokia Corporation. All rights are reserved. Copying, including
- * reproducing, storing, adapting or translating, any or all of this material
- * requires the prior written consent of Nokia Corporation. This material also
- * contains confidential information which may not be disclosed to others
- * without the prior written consent of Nokia.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,     
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER  
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS  
+ * IN THE SOFTWARE. 
  */
 
 #include "qmlshareui.h"
